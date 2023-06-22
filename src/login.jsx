42ba7c9372eee30e2b347274bf0e8f46f6f75f93
Alexgualtero <alexandergualtero@hotmail.com>
2023-06-22 12:40:35 -0500

    creando form

---
+++ src/login.jsx
@@ -0,0 +1,23 @@
+export function Login(){
+
+    return (
+    
+        <div>
+
+            <h1 className="titulo1">TITULO EMPRESA</h1>
+
+            <form>
+                <label>
+                     Usuario:
+                     <input type="text" name="name" />
+                </label>
+            <input type="submit" value="Enviar" />
+            </form>
+
+        </div>
+    
+    );
+
+    
+
+}
