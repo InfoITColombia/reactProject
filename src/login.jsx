673fe928fe914c2653da51772d31060f1fad94e5
Alex Gualtero <alexandergualtero@hotmail.com>
2023-06-22 22:51:29 -0500

    modificaciones con Bootstrap

--- src/login.jsx
+++ src/login.jsx
@@ -1,23 +1,32 @@
+import { Button } from 'react-bootstrap';
+import Form from 'react-bootstrap/Form';
+
 export function Login(){
 
     return (
     
         <div>
 
-            <h1 className="titulo1">TITULO EMPRESA</h1>
+            <h1 className="titulo1">TOUCH CENTER</h1>
 
-            <form>
-                <label>
-                     Usuario:
-                     <input type="text" name="name" />
-                </label>
-            <input type="submit" value="Enviar" />
-            </form>
+            <Form className='formLogin'>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Control type="text" placeholder="Usuario" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formPlaintextPassword">
+                <Form.Control type="password" placeholder="Contraseña" />
+                </Form.Group>
+                <div className='button'>
+                <Button variant='primary'>Ingresar</Button>
+                </div>
+                
+            </Form>
+
+            
 
         </div>
     
     );
 
-    
-
 }
+
