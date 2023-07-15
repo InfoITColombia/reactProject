4d456579fb2ecaec945c750f50cffdda8dc1bb18
Developer <miguellperezzv@gmail.com>
2023-07-15 14:42:39 -0500

    Validación login con swal alerts

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,83 +1,105 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Axios from 'axios'
 
 import { Button } from 'react-bootstrap';
 import Form from 'react-bootstrap/Form';
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2'
+import withReactContent from 'sweetalert2-react-content'
 
 
 
 
-export function Login(){
+export function Login() {
 
-  const [usernameReg,setUsernameReg] = useState("")
-  const [passwordReg,setPasswordReg] = useState("")
+  const [usernameReg, setUsernameReg] = useState("")
+  const [passwordReg, setPasswordReg] = useState("")
 
-  const [username,setUsername] = useState("")
-  const [password,setPassword] = useState("")
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
 
   const [loginStaus, setLoginStatus] = useState("");
 
+  const navigate = useNavigate();
+  const navigateVentas = () => {
+    //  navigate a /ventas
+    navigate('/ventas');
+  };
 
-  const register = ()=>{
-    Axios.post("http://localhost:3002/register",{
-    username: usernameReg,
-    password: passwordReg,
-  }).then((response)=>{
-    console.log(response)
-  });
+
+  const register = () => {
+    Axios.post("http://localhost:3002/register", {
+      N_USUARIO: usernameReg,
+      PWD_USUARIO: passwordReg,
+    }).then((response) => {
+      console.log(response)
+    });
   }
 
 
-  const login = ()=>{
-    Axios.post("http://localhost:3002/login",{
-    username: username,
-    password: password,
-  }).then((response)=>{
-    if(response.data.message){
-      setLoginStatus(response.data.message)
-    }
-    else{
-      setLoginStatus("Inicio válido!!")
-    }
-      //console.log(response.data)
-  });
-}
-    
+  const login = () => {
+    console.log(username)
+    console.log(password)
+    Axios.post("http://localhost:3002/login", {
+      N_USUARIO: username,
+      PWD_USUARIO: password,
+    }).then((response) => {
+      if (response.data.message) {
+        //Variable que se carga al html
+        //setLoginStatus(response.data.message)
+        const MySwal = withReactContent(Swal)
 
-    return (
-
-          
-        <div> 
-
-
-            <h1 className="titulo1">TOUCH CENTER</h1>
-
-            <Form className='formLogin'>
-                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                
-                <Form.Control type="text" placeholder="Usuario" onChange={(e)=>{
-                  setUsername(e.target.value);
-                }} />
-                </Form.Group>
-                <Form.Group className="mb-3" controlId="formPlaintextPassword">
-                
-                <Form.Control type="password" placeholder="Contraseña" onChange={(e)=>{
-                  setPassword(e.target.value);
-                }} />
-                </Form.Group>
-                <div className='button'>
-                
-                <Button variant='primary' onClick={login}>Ingresar</Button>
-                </div>
-
-            </Form>
+        MySwal.fire({
+          title: <strong>Credenciales incorrectas</strong>,
+          html: <i>{response.data.message}</i>,
+          icon: 'error'
+        })
+      }
+      else {
+        console.log("LOGIN EXITOSO")
+        
+        //direccionamiento a ventas
+        navigateVentas()
+      }
+      
+    });
+  }
 
 
-            <h1>{loginStaus}</h1>
+  return (
 
+
+    <div>
+
+
+      <h1 className="titulo1">TOUCH CENTER</h1>
+
+      <Form className='formLogin'>
+        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+
+          <Form.Control type="text" placeholder="Usuario" onChange={(e) => {
+            setUsername(e.target.value);
+          }} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formPlaintextPassword">
+
+          <Form.Control type="password" placeholder="Contraseña" onChange={(e) => {
+            setPassword(e.target.value);
+          }} />
+        </Form.Group>
+        <div className='button'>
+
+          <Button variant='primary' onClick={login}>Ingresar</Button>
         </div>
-    
-    );
+
+      </Form>
+
+
+      <h1>{loginStaus}</h1>
+
+    </div>
+
+  );
 
 }
 
