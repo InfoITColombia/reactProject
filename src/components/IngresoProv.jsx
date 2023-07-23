bbcf86d69a3688218cfcaa58b1ff698c15d1c244
miguellperezzv <42900928+miguellperezzv@users.noreply.github.com>
2023-07-22 22:54:08 -0500

    Tabla con proveedores

--- src/components/IngresoProv.jsx
+++ src/components/IngresoProv.jsx
@@ -1,10 +1,20 @@
 import Table from 'react-bootstrap/Table';
+import React, { useState, useEffect } from 'react';
 import { Button } from 'react-bootstrap';
 import logo from '../img/logotouch.png';
 
 
 export function IngresoProv () {
 
+  const [proveedores, setProveedores] = useState([]);
+
+  useEffect(() => {
+    
+    fetch('http://localhost:3002/touchapp/proveedor/') 
+      .then((response) => response.json())
+      .then((data) => setProveedores(data))
+      .catch((error) => console.error('Error al obtener los proveedores:', error));
+  }, []);
 
   return (
 
@@ -16,36 +26,37 @@ export function IngresoProv () {
 
       <section className='table1'>
 
-    <Table  striped bordered hover>
-      <thead >
-        <tr>
-          <th>#</th>
-          <th>Nombre Empresa</th>
-          <th>Nit </th>
-          <th>Email</th>
-          <th>Dirección</th>
-          <th>Telefono</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Apple</td>
-          <td>988765128</td>
-          <td>comercial@apple.com</td>
-          <td>1234 Florida</td>
-          <td>3002986345</td>
-          <td>
-          <div className='button'>
-                <Button className='btn1' variant='primary'>Editar</Button>
-                <Button className='btn2' variant='danger'>Eliminar</Button>
-          </div>
-          </td>
-        </tr>
-                
-      </tbody>
-      
-    </Table>
+      <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Nombre Empresa</th>
+              <th>Nit</th>
+              <th>Email</th>
+              <th>Dirección</th>
+              <th>Telefono</th>
+              <th>Acciones</th>
+            </tr>
+          </thead>
+          <tbody>
+            {proveedores.map((proveedor, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{proveedor.N_PROVEEDOR}</td>
+                <td>{proveedor.NIT_PROVEEDOR}</td>
+                <td>{proveedor.EMAIL_PROVEEDOR}</td>
+                <td>{proveedor.DIR_PROVEEDOR}</td>
+                <td>{proveedor.TEL_PROVEEDOR}</td>
+                <td>
+                  <div className='button'>
+                    <Button className='btn1' variant='primary'>Editar</Button>
+                    <Button className='btn2' variant='danger'>Eliminar</Button>
+                  </div>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
     </section>
     </div>
   );
